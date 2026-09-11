32f386171eab1d7ab3a12572f109cf2d522d1bf7
WangZhanze <wangzhanze@huoban.ai>
2026-09-11 11:14:20 +0800

    Handle version strings with existing V prefix

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -25,7 +25,7 @@ export function AppHeader({ activePage, enabled, onBrandClick, onNavigate, onTog
         <img src={appIconUrl} alt="" width={28} height={28} style={{ flexShrink: 0, objectFit: 'contain' }} />
         <span>
           <span className="brand-name">axonkey</span>
-          <span className="brand-version"><span>V{appPackage.version}</span></span>
+          <span className="brand-version"><span>{/^[vV]/.test(appPackage.version) ? appPackage.version : `V${appPackage.version}`}</span></span>
         </span>
       </button>
       <div className="title-row"><h1>{pageTitles[activePage]}</h1></div>
