37db0231f1e704cc42ef1d8377742f778de063cd
leo03w <leo03wzz@gmail.com>
2026-09-12 16:25:53 +0800

    feat: add mouse button behavior backend

--- src-tauri/src/input_service/mod.rs
+++ src-tauri/src/input_service/mod.rs
@@ -110,7 +110,11 @@ impl NativeBehavior {
 
 #[derive(Clone, Copy, Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub(super) enum MouseButton { Left, Middle, Right }
+pub(super) enum MouseButton {
+    Left,
+    Middle,
+    Right,
+}
 
 #[cfg(target_os = "macos")]
 mod macos;
